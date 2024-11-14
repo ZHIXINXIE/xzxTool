c3fbc6aacaa781fc5bf51a38c5b210a3c5fd989e
zhixinxie <ZHIXIN001@e.ntu.edu.sg>
2024-11-14 13:59:32 +0800

    be a python package

--- setup.py
+++ setup.py
@@ -1,20 +1,3 @@
-from setuptools import setup, find_packages
+import setuptools
 
-setup(
-    name='xzxTool',
-    version='0.1',
-    packages=find_packages(),
-    install_requires=[
-        "transformers",
-        "torch",
-        "numpy",
-        "pandas",
-    ],
-    entry_points={
-        'console_scripts': [
-        ],
-    },
-    # 其他元数据，例如作者、描述等
-    author='Zhixin Xie',
-    description='For self use',
-)
+setuptools.setup()
